abf462d31aeab864c896f09b4158b03affaacfd5
VibhinnS <vibhinnsinghal25@gmail.com>
2024-07-25 15:18:56 +0530

    Added YAML script to run Docker image

--- fireside/helpers/env.py
+++ fireside/helpers/env.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from functools import lru_cache
 from decouple import Config, RepositoryEnv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,6 +9,7 @@ REPO_DIR = BASE_DIR.parent
 REPO_DIR_ENV = REPO_DIR / '.env'
 REPO_DIR_WEB_ENV = BASE_DIR / '.env.web'
 
+@lru_cache
 def get_config():
     if BASE_DIR_ENV.exists():
         return Config(RepositoryEnv(BASE_DIR_ENV))
